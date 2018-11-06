91c0a9ce9a4f31ca42cb628de530356c94d0dd5c
Jonas Dieterle <jonas.dieterle@gmail.com>
2018-11-06 11:14:43 +0800

    Your heart's been aching but you're too shy to say it

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #Cocktail
+  # get 'cocktails/new', to: 'cocktails#new', as: 'new_cocktail'
+  # post 'cocktails/', to: 'cocktails#create', as: 'post_cocktail'
+  # get 'cocktails/', to: 'cocktails#index', as: 'cocktails'
+  # get 'cocktails/:id', to: 'cocktails#show', as: 'cocktail'
+  # #Dose
+  # get 'doses/new', to: 'doses#new', as: 'new_dose'
+  # post 'doses/', to: 'doses#create', as: 'post_dose'
+
+  resources :cocktails do
+    resources :doses
+  end
+
+  # resources :ingredients do
+  #   resources :doses
+  # end
+
 end
+
+
